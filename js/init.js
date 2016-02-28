8412c63cd299ec1970a961a765e70a7b05f76734
L Nawrocki <lwn2@aol.com>
2016-02-28 00:10:17 -0500

    edits to style

--- js/init.js
+++ js/init.js
@@ -1,15 +1,10 @@
-/*
- * Made by WebDesignCrowd
- * http://webdesigncrowd.com
- *
- */
- 
+
 (function($){
 	$(function(){
-	  
+
     var navbarHeight = 80;
 
-    // Slide in Functionality 
+    // Slide in Functionality
     $(window).scroll(function() {
       var top = $(window).scrollTop();
       $(".slide-in").each(function () {
@@ -18,7 +13,7 @@
         if ((top > (thisTop - (height * 1.5))) && !$(this).hasClass("slid")) {
           $(this).addClass("slid");
         }
-      });   
+      });
       $('body').scrollspy({ offset: navbarHeight+10, target: '#navbar' });
     });
 
@@ -36,7 +31,7 @@
         }
       }
     })
-    
+
 
     // Parallax Scripts
     function updateParallax() {
@@ -48,20 +43,20 @@
           var scrollTop = $(window).scrollTop() + windowHeight;
           var fromTop = 0;
           var isHome = true;
-          if (top === 0) { 
-            fromTop = $(window).scrollTop() - top; 
+          if (top === 0) {
+            fromTop = $(window).scrollTop() - top;
             isHome = true;
           }
-          else { 
-            fromTop = $(window).scrollTop() - top + windowHeight; 
+          else {
+            fromTop = $(window).scrollTop() - top + windowHeight;
             isHome = false;
           }
-          if ((bottom > $(window).scrollTop()) && (top < scrollTop)) {   
+          if ((bottom > $(window).scrollTop()) && (top < scrollTop)) {
             var parallax = -1 * (fromTop / 3);
             var revParallax = parallax;
             var percent = 1 - 1.3 *($(window).scrollTop() / $(window).height());
-            if (isHome) { 
-              revParallax += navbarHeight; 
+            if (isHome) {
+              revParallax += navbarHeight;
               $("#home .logo").css('marginTop', parallax + "px");
               $("#home .logo, #home .welcome, #home .call-to-action, #home .macbook-preview").css('opacity', percent);
             }
@@ -71,10 +66,10 @@
       }
     }
     updateParallax();
-    
+
     $(window).scroll(function() {
       updateParallax();
-      // if ($(window).height() > $(window).scrollTop()) {   
+      // if ($(window).height() > $(window).scrollTop()) {
       //   var parallax = -1 * ($(window).scrollTop() / 3);
       //   var revParallax = navbarHeight + parallax;
       //   var percent = 1 - 1.5 *($(window).scrollTop() / $(window).height());
@@ -83,8 +78,8 @@
       //   $("#home .logo, #home .welcome, #home .call-to-action").css('opacity', percent);
       // }
     });
-    
-      
+
+
     // Contact Form Icon
     $("form .form-control").focus(function() {
       $(this).siblings("label").first().children("i").first().css({"color": "#aaa", "left": 0});
@@ -92,10 +87,10 @@
     $("form .form-control").blur(function() {
       $(this).siblings("label").first().children("i").first().css({"color": "transparent", "left": "-20px"});
     });
-	  
+
     // Blog Masonry
     var $container = $('.masonry-grid');
-    
+
     $container.imagesLoaded(function(){
       new AnimOnScroll( document.getElementById( 'grid' ), {
         minDuration : 0.4,
@@ -112,13 +107,13 @@
     });
 
 
-    // Accordion Active Toggling 
+    // Accordion Active Toggling
     $("a[data-toggle='collapse']").click(function() {
       if ($(this).parent().parent(".panel-heading").hasClass("active")) {
         $(this).parent().parent().parent().parent().find('.panel-heading').removeClass("active");
       }
       else {
-        $(this).parent().parent().parent().parent().find('.panel-heading').removeClass("active");  
+        $(this).parent().parent().parent().parent().find('.panel-heading').removeClass("active");
         $(this).parent().parent(".panel-heading").addClass("active");
       }
     });
@@ -145,8 +140,8 @@
 
       }
       google.maps.event.addDomListener(window, 'load', initialize);
-    
-    
+
+
 
 	}); // end of document ready
 })(jQuery); // end of jQuery name space
